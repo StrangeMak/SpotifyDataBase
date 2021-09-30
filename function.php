0fafe02063153461b1caef9baf587dbbd5c04e4c
StrangeMak <75211566+StrangeMak@users.noreply.github.com>
2021-09-30 16:43:55 +0200

    Add files via upload

---
+++ function.php
@@ -0,0 +1,73 @@
+<?php
+
+//Fonction qui permet de se connexionecter à la base de données
+function ConnectDataBase($server, $user, $mdp, $db) 
+{
+    $connexion = mysqli_connect($server, $user, $mdp, $db); // serveur, login, mdp, bd
+    if (mysqli_connect_errno()) {
+        exit();
+    }
+    mysqli_query($connexion,'SET NAMES UTF8'); // conversion chaines en UTF8 (si problème affichage)
+    return $connexion;
+}
+
+
+function recuperationData()
+{
+    global $connexion;
+    $filename = "Spotify-2000.csv";
+    $file = fopen($filename, "r");
+    fgets($file);
+    while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
+
+        $sqlInsert = "INSERT into test (Id,Title,Artist,TopGenre,Year,BPM,Energy,Danceability,Loudness,
+                Liveness,Valence,Length,Acousticness,Speechiness,Popularity)
+                values ('" . $column[0] . "','" . $column[1] . "','" . $column[2] . "','" . $column[3] . "','" . $column[4] ."','" .
+                 $column[5] ."','" . $column[6] ."','" . $column[7] ."','" . $column[8] ."','" . $column[9] ."','" . $column[10] ."','" .
+                  $column[11] ."','" . $column[12] ."','" . $column[13] ."','" . $column[14] . "')"; 
+
+        $result = mysqli_query($connexion, $sqlInsert);        
+                    
+        if (! empty($result)) {
+            $type = "success";
+            $message = "CSV Data Imported into the Database";
+        } else {
+            $type = "error";
+            $message = "Problem in Importing CSV Data";
+        }
+    }
+}
+
+/*function trieData();*/
+function affichageData(){
+    global $connexion;
+    $sql = "SELECT * from test";
+    $result = mysqli_query($connexion, $sql);
+    $all_property = array();
+
+    $html = '';
+	$html .= '<table class="table table-bordered table-striped table-hover">
+				<thead class="table-dark">
+					<tr>';
+    while ($property = mysqli_fetch_field($result)){
+        $html .= '<td>' . $property->name . '</td>';  
+        array_Push($all_property, $property->name); 
+    }
+    $html .= '</tr>
+            </thead>
+            <tbody>';
+    while ($row = mysqli_fetch_array($result)) {
+        $html .= "<tr>";
+        foreach ($all_property as $item) {
+            $html .= '<td>' . $row[$item] . '</td>';
+        }
+        $html .= '</tr>';
+    }
+    $html .= '	</tbody>
+			 </table>';
+
+    echo $html;
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+    include 'function.php';
+    $SERVER = "localhost";            
+    $USER   = "loic";                 
+    $MDP    = "loic";                  
+    $BD     = "spotify";
+
+    echo "<br><br><br>";
+    $connexion = ConnectDataBase($SERVER, $USER, $MDP, $BD); // connexion à la BD
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+		rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Classement musiques Spotify</title>
+  </head>
+  <body style="background-color: #f4efe7">
+  <header>
+    <nav class="navbar py-0 navbar-expand-lg fixed-top navbar-dark" style="background-color: black;">
+      <a class="navbar-brand" href="index.php">
+        <img src="images/Spotify.jpg" width="95" height="54" alt="Info Logo" />
+      </a>
+      <span class="navbar-text ms-3" style="color: white; font-weight: bold; font-size: 1em;"> Meilleures musiques Spotify </span>
+      <ul class="nav justify-content-center">
+        <li class="nav-item ms-2">
+          <a class="nav-link text-white" href="#">Accueil</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+<?php
+    //recuperationData();
+    affichageData();
+?>
+
+
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
